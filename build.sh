6a086e77e1a6d00e41b5dcd84a9015c5f9d68ffd
Chris Bartlett <bartlettc@gmail.com>
2018-05-09 13:30:56 -0600

    Updated deploy to Github releases

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@
 VERSION=master
 GOOS=linux
 
+# Directory to house our binaries
+mkdir -p bin
+
 docker build --build-arg VERSION=${VERSION} --build-arg GOOS=${GOOS} -t readytalk/vault-admin:binary-build ./
 
 docker run --rm --name vault-admin-build -d readytalk/vault-admin:binary-build sh -c "sleep 120"
 
-docker cp vault-admin-build:/usr/bin/vadmin ./
+docker cp vault-admin-build:/usr/bin/vadmin bin
 docker stop vault-admin-build
 
-zip vadmin-${VERSION}.zip vadmin
-vadmin --version
+cd bin
+zip bin/vadmin-${GOOS}-${VERSION}.zip bin/vadmin
+rm bin/vadmin
