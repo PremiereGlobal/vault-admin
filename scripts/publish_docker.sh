678623095055366be96643dde365869b870f6850
Chris Bartlett <bartlettc@gmail.com>
2018-05-09 16:41:31 -0600

    Fixing docker build

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -2,6 +2,6 @@
 
 VERSION=${1:-master}
 
-docker tag ${DOCKER_REPO}:${VERSION}-${GOOS}-linux ${DOCKER_REPO}:${VERSION}
+docker tag ${DOCKER_REPO}:${VERSION}-linux ${DOCKER_REPO}:${VERSION}
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${DOCKER_REPO}:${VERSION}
